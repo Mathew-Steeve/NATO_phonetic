831bf9e699c6d2c0985801924eb4be0f48cd6995
Mathew_Steeve <mthwsteeve@gmail.com>
2024-08-09 13:23:56 +0530

    Exception handling

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import pandas
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-#TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 new_dict = {word.letter: word.code for (index, word) in data.iterrows()}
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-# dict = pandas.DataFrame(new_dict)
-word = input("Enter the word : ").upper()
-new_list = [new_dict[letter] for letter in word]
-print(new_list)
+# while KeyError:
+
+
+def generate():
+    word = input("Enter the word : ").upper()
+    try:
+        new_list = [new_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please. ")
+        generate()
+    else:
+        print(new_list)
+        # break
+
+
+generate()
